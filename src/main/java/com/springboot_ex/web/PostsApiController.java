76655462fb4a156aaa65856978f8e43d816446ab
hyejeongjang <hj970219@naver.com>
2021-01-07 23:52:39 +0900

    Posts API 제작

---
+++ src/main/java/com/springboot_ex/web/PostsApiController.java
@@ -0,0 +1,18 @@
+package com.springboot_ex.web;
+import com.springboot_ex.service.PostsService;
+import com.springboot_ex.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+}
